5490e9de41ced08fee5d0213cb43af3882749e86
Bonuspunkt <bonuspunkt+github@gmail.com>
2015-02-26 22:38:23 +0100

    refactored everything - extracted settings to a config file - extracted handlers

--- bot.js
+++ bot.js
@@ -1,21 +1,25 @@
+#!/usr/bin/env node
 var irc = require('irc');
-
 var registry = require('./lib/registry');
+var settings = require('./settings');
 
-var client = new irc.Client('irc.quakenet.org', 'reflexDeBot', {
-  debug: true,
-  retryDelay: 30000,
-  channels: ['#reflex.de']
-});
+// load registry
+if (settings.handlers) {
+  Object.keys(settings.handlers).forEach(function(key) {
+    registry.register(key, settings.handlers[key]);
+  });
+}
 
+console.log('starting')
+var client = new irc.Client(settings.server, settings.nick, settings.options);
 
 client.addListener('message', function (from, to, message) {
-
+  console.log('got message', from, to, message);
   try {
     registry.handle({
       client: client,
       to: /^#/.test(to) ? to : from,
-      message: message.toLowerCase().trim()
+      message: message
     });
   } catch(e) {
     console.log(e);
